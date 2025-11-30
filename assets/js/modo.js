b22250791e70b2113bc550a4683b824bff01d587
Supremeduck008 <pedro.otaviobraga@gmail.com>
2025-11-30 17:09:14 -0300

    feat:adiciona js na header

--- assets/js/modo.js
+++ assets/js/modo.js
@@ -1,1 +1,32 @@
 
+const btn = document.getElementById("btn-tema")
+const header = document.getElementById("header")
+const temaSalvo = localStorage.getItem("tema");
+
+if (temaSalvo) {
+    header.classList.remove("claro", "escuro"); 
+    header.classList.add(temaSalvo);
+}
+
+
+
+
+
+
+
+
+
+if (btn) {
+    btn.addEventListener("click", () => {
+        if (header.classList.contains("claro")) {
+            header.classList.replace("claro", "escuro");
+            localStorage.setItem("tema", "escuro"); 
+        } else {
+            header.classList.replace("escuro", "claro");
+            localStorage.setItem("tema", "claro");
+        }
+    });
+}
+
+
+
